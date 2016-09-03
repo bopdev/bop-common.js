af68e54e6b9e71c295d561abd23c46b59f5c09cb
JimmySevere <JimmySevere@users.noreply.github.com>
2016-09-03 20:20:47 +0100

    Massive reduction in size

--- functions/dismiss.js
+++ functions/dismiss.js
@@ -1,33 +1,13 @@
-$('body').on('click', '[data-bop-dismiss]', function(e){
+$('body').on('click.bop.dismiss', '[data-bop-dismiss]', function(e){
 	e.preventDefault();
 	var $btn = $(this);
 	var sel = $btn.data('bop-dismiss');
-	var $dismiss = $dismissBtn.parents(sel);
+	var $dismiss = $btn.closest(sel);
+	$dismiss = $dismiss.length ? $dismiss : $(sel);
 	
-	if($dismiss.length){
-		$dismiss.css('display', 'none');
-		return;
-	}
-	
-	$dismiss = $btn.parent().find(sel);
-	if($dismiss.length){
+	if($btn.hasClass('bop-dismiss-with-wiping')){
+		$dismiss.remove();
+	}else{
 		$dismiss.css('display', 'none');
 	}
-	
-}).on('click', '[data-bop-dismiss-with-wiping]', function(e){
-	e.preventDefault();
-	var $btn = $(this);
-	var sel = $btn.data('bop-dismiss-with-wiping');
-	var $dismiss = $btn.parents(sel);
-	
-	if($dismiss.length){
-		$dismiss.remove();
-		return;
-	}
-	
-	$dismiss = $btn.parent().find(sel);
-	if($dismiss.length){
-		$dismiss.remove();
-	}
-	
 });
